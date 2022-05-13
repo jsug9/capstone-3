57628203c333f553e06285680328fcd0cfed0beb
Augusto <jesus_augga_99@icloud.com>
2022-05-12 22:04:31 -0500

    fix style not loading in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './Style/index.scss';
+import './Styles/index.scss';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
