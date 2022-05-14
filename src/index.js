e93dd31e8b256d50558f03ec3c7a2080d73cb5e3
Augusto <jesus_augga_99@icloud.com>
2022-05-14 13:20:04 -0500

    linter fix test

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import './Styles/Style.scss';
-import store from './Redux/configureStore';
+import store from './configureStore';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
