9668f9da1d98e1ca2329216985ce26bf44db9c9d
Augusto <jesus_augga_99@icloud.com>
2022-05-14 12:30:07 -0500

    change index.scss to style.scss for a better understanding

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import './Styles/index.scss';
+import './Styles/Style.scss';
 import App from './App';
 import store from './Redux/configureStore';
 
