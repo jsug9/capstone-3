563198989e799a58260d57c07de912fe52c2de50
Augusto <jesus_augga_99@icloud.com>
2022-05-17 15:59:57 -0500

    add styling to country list

--- src/Components/CountriesList.js
+++ src/Components/CountriesList.js
@@ -17,12 +17,29 @@ const CountriesList = () => {
       name: 'Spain',
       cases: 1200,
     },
+    {
+      id: '4',
+      name: 'Argentina',
+      cases: 1200,
+    },
+    {
+      id: '5',
+      name: 'France',
+      cases: 1200,
+    },
+    {
+      id: '6',
+      name: 'China',
+      cases: 1200,
+    },
   ];
 
   return (
-    countries.map((country) => (
-      <CountryItem key={country.id} country={country} />
-    ))
+    <div className="countriesList">
+      {countries.map((country) => (
+        <CountryItem key={country.id} country={country} />
+      ))}
+    </div>
   );
 };
 
