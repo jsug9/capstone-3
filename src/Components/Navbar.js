d983d718c0d7925938d380c9f32a21d15eaf55dc
Augusto <jesus_augga_99@icloud.com>
2022-05-17 18:09:46 -0500

    add hover effects to navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,11 +3,11 @@ import { Icon } from '@iconify/react';
 
 const Navbar = () => (
   <nav>
-    <Icon icon="eva:arrow-ios-back-fill" width="30" height="30" />
+    <Icon icon="eva:arrow-ios-back-fill" width="30" height="30" className="navbarHover" />
     <h1>Covid Tracker</h1>
     <div className="left-nav">
-      <Icon icon="fluent:mic-16-filled" width="30" height="30" />
-      <Icon icon="ci:settings-filled" width="30" height="30" />
+      <Icon icon="fluent:mic-16-filled" width="30" height="30" className="navbarHover" />
+      <Icon icon="ci:settings-filled" width="30" height="30" className="navbarHover" />
     </div>
   </nav>
 );
