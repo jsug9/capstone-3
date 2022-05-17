d9436d49c4ca551517df5803ab027f2575e46c1d
Augusto <jesus_augga_99@icloud.com>
2022-05-17 11:05:01 -0500

    add getregions function

--- src/Redux/regions/RegionsReducer.js
+++ src/Redux/regions/RegionsReducer.js
@@ -1,15 +1,26 @@
+import { fetchRegions } from '../../API/CovidAPI';
+
 const GET_REGIONS = 'Capstone-3/countries/GET_REGIONS';
 
 const initialState = [];
 
+const getRegions = (country) => async (dispatch) => {
+  const result = await fetchRegions(country);
+  dispatch({
+    type: GET_REGIONS,
+    payload: result,
+  });
+};
+
 const regionsReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_REGIONS:
-      return state;
+      return action.payload;
 
     default:
       return state;
   }
 };
 
+export { getRegions };
 export default regionsReducer;
