3d340f9a32c87c6d96f613923dda8cd93d111d95
Augusto <jesus_augga_99@icloud.com>
2022-05-17 17:50:30 -0500

    add icon to country item

--- src/Components/CountryItem.js
+++ src/Components/CountryItem.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Icon } from '@iconify/react';
 
 const CountryItem = (props) => {
   const { country } = props;
 
   return (
     <div className="countryItem">
-      <p>{country.name}</p>
-      <p>{country.cases}</p>
+      <Icon icon="carbon:location-filled" width="50" height="50" className="mapIcon" />
+      <div className="countryInformation">
+        <p className="countryName">{country.name}</p>
+        <p>{country.cases}</p>
+      </div>
     </div>
   );
 };
