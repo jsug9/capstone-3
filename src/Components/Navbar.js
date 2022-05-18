a1afb6b2ff3a88425f7a3fac9018ae373a6f98a1
Augusto <jesus_augga_99@icloud.com>
2022-05-17 21:52:39 -0500

    add navlink to navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import { Icon } from '@iconify/react';
 
 const Navbar = () => (
   <nav>
-    <Icon icon="eva:arrow-ios-back-fill" width="30" height="30" className="navbarHover" />
+    <NavLink to="/">
+      <Icon icon="eva:arrow-ios-back-fill" width="30" height="30" className="backArrow navbarHover" />
+    </NavLink>
     <h1>Covid Tracker</h1>
     <div className="left-nav">
       <Icon icon="fluent:mic-16-filled" width="30" height="30" className="navbarHover" />
