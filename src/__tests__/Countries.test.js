05461fe501359a85c5029a6643d9888f439534d7
Augusto <jesus_augga_99@icloud.com>
2022-05-19 12:18:35 -0500

    add test for individual elements

--- src/__tests__/Countries.test.js
+++ src/__tests__/Countries.test.js
@@ -29,3 +29,34 @@ describe('Get Countries', () => {
     expect(result).toEqual(state);
   });
 });
+
+describe('Check element id', () => {
+  test('When Get Countries is executed, the id of an element should match the one in the initial state', () => {
+    const initialState = [];
+    const state = [
+      {
+        id: 'peru',
+        name: 'Peru',
+        image: 'https://api.covid19tracking.narrativa.com/api/peru',
+        today_confirmed: '300,201',
+      },
+      {
+        id: 'ecuador',
+        name: 'Ecuador',
+        image: 'https://api.covid19tracking.narrativa.com/api/ecuador',
+        today_confirmed: '100,205',
+      },
+      {
+        id: 'brazil',
+        name: 'Brazil',
+        image: 'https://api.covid19tracking.narrativa.com/api/brazil',
+        today_confirmed: '600,221',
+      },
+    ];
+
+    const action = { type: GET_COUNTRIES, payload: state };
+    const result = countriesReducer(initialState, action);
+    const ecuador = state[1].id;
+    expect(result[1].id).toEqual(ecuador);
+  });
+});
